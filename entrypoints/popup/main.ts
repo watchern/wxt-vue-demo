aa5f31b397cd39acf6a2a07c8fc5164e1ac5b189
Rpan <watchern@126.com>
2026-02-07 15:32:40 +0800

    refactor(几个文件): 增加options页面

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
+const app = createApp(App)
+app.use(i18n)
 
-createApp(App).use(i18n).mount('#app');
+app.mount('#app');
